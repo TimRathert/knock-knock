08129fdeed23d97be1928f70843e5e69fde6172e
TimRathert <rathertt@gmail.com>
2022-10-17 14:46:47 -0400

    working on env

--- knock-knock/next.config.js
+++ knock-knock/next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {},
+  env: {
+    SETUP_URL: process.env.NEXT_PUBLIC_SETUP_URL,
+    NEWJOKE_URL: process.env.NEXT_PUBLIC_NEWJOKE_URL,
+    FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    APPID: process.env.APPID,
+  },
 }
 
 module.exports = nextConfig
